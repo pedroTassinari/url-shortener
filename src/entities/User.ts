e92071905455333b80278a8f47e46a363f372e1a
Pedro Tassinari <pedro.tassinari99@gmail.com>
2025-05-06 20:30:08 -0300

    feat: add users entity

---
+++ src/entities/User.ts
@@ -0,0 +1,48 @@
+import {
+	Column,
+	CreateDateColumn,
+	DeleteDateColumn,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	PrimaryColumn,
+	UpdateDateColumn,
+} from 'typeorm';
+import { v7 as uuid } from 'uuid';
+
+import { Tenant } from './Tenant';
+
+@Entity({ name: 'users' })
+export class User {
+	@CreateDateColumn({ name: 'created_at' })
+	createdAt: Date;
+
+	@DeleteDateColumn({ name: 'deleted_at' })
+	deletedAt?: Date;
+
+	@Column({ unique: true })
+	email: string;
+
+	@PrimaryColumn()
+	id: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	password: string;
+
+	@JoinColumn({ name: 'tenant_id' })
+	@ManyToOne(() => Tenant)
+	tenant: Tenant;
+
+	@Column({ name: 'tenant_id' })
+	tenantId: string;
+
+	@UpdateDateColumn({ name: 'updated_at' })
+	updatedAt?: Date;
+
+	constructor() {
+		this.id = uuid();
+	}
+}
