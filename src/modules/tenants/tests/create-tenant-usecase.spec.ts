067ab3acdbad47d29e0675267a2759d465c5b45c
Pedro Tassinari <pedro.tassinari99@gmail.com>
2025-05-06 21:30:35 -0300

    test: adjust create tenant unit tests

--- src/modules/tenants/tests/create-tenant-usecase.spec.ts
+++ src/modules/tenants/tests/create-tenant-usecase.spec.ts
@@ -1,12 +1,10 @@
-import { hash } from 'bcrypt';
-import { v4 as uuid } from 'uuid';
+import { createId } from '@paralleldrive/cuid2';
 
 import { Tenant } from '../../../entities/Tenant';
 import { CreateTenantUseCase } from '../create-tenant-usecase';
 import { ITenantRepository } from '../repositories/tenant-repository';
 
-jest.mock('bcrypt');
-jest.mock('uuid');
+jest.mock('@paralleldrive/cuid2');
 
 describe('CreateTenantUseCase', () => {
 	let createTenantUseCase: CreateTenantUseCase;
@@ -15,6 +13,7 @@ describe('CreateTenantUseCase', () => {
 	beforeEach(() => {
 		tenantRepository = {
 			createTenant: jest.fn(),
+			findByApiKey: jest.fn(),
 		};
 		createTenantUseCase = new CreateTenantUseCase(tenantRepository);
 	});
@@ -22,18 +21,14 @@ describe('CreateTenantUseCase', () => {
 	it('should create a tenant and return it with the plain API key', async () => {
 		const name = 'Test Tenant';
 		const apiKey = 'plain-api-key';
-		const hashedApiKey = 'hashed-api-key';
-		const tenant: Tenant = { apiKey: hashedApiKey, createdAt: new Date(), id: '1', name };
+		const tenant: Tenant = { apiKey, createdAt: new Date(), id: '1', name };
 
-		(uuid as jest.Mock).mockReturnValue(apiKey);
-		(hash as jest.Mock).mockResolvedValue(hashedApiKey);
 		tenantRepository.createTenant.mockResolvedValue(tenant);
+		(createId as jest.Mock).mockReturnValueOnce(apiKey);
 
 		const result = await createTenantUseCase.execute(name);
 
-		expect(uuid).toHaveBeenCalled();
-		expect(hash).toHaveBeenCalledWith(apiKey, 10);
-		expect(tenantRepository.createTenant).toHaveBeenCalledWith({ apiKey: hashedApiKey, name });
-		expect(result).toEqual({ ...tenant, apiKey });
+		expect(tenantRepository.createTenant).toHaveBeenCalledWith({ apiKey, name });
+		expect(result).toEqual({ ...tenant });
 	});
 });
